66780a0a62967e926b307e4094d1747db274eb70
Maria Belova <bmvria357@gmail.com>
2023-10-19 21:54:48 +0300

    Добавлены IngredientTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -17,24 +17,21 @@ public class BunTest {
         bun = new Bun(name, price);
     }
 
-    @Parameterized.Parameters(name = "Тестовые данные: {0}, {1}")
+    @Parameterized.Parameters(name = "Тестовые данные: наименование \"{0}\"; Стоимость: {1}")
     public static Object[][] dataForTest() {
         return new Object[][]{
-                {"Космос", 1000},
-                {"Space", 11.1f},
-                {"Луна", -555},
-                {"облачко", 0},
-                {"пыль", -0},
+                {"Флюоресцентная булка R2-D3", 998},
+                {"Краторная булка N-200i", 116.1f},
         };
     }
 
     @Test
-    public void getNameBun() {
+    public void getNameBunTest() {
         Assert.assertEquals(name, bun.getName());
     }
 
     @Test
-    public void getPriceBun() {
+    public void getPriceBunTest() {
         Assert.assertEquals(price, bun.getPrice(), 0.1);
     }
 }
