03f0c9550010ed0c36108440ca077fc58c07e4eb
Maria Belova <bmvria357@gmail.com>
2023-10-20 19:26:59 +0300

    Редактирование gitignore

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -1,5 +1,23 @@
 package praktikum;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    
+    @Mock
+    private Burger burger;
+
+    @Test
+    public void SetBunsShouldCallOnce() {
+        Bun bun = new Bun("Кунжутная", 100);
+        //burger.setBuns(bun);
+        Assert.assertEquals(
+                bun.getName(),
+                burger.bun.getName());
+    }
 }
